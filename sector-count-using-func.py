cd3d5cba3eb022bd9485539983ec5b7553aee350
khai <quangkhai338@gmail.com>
2018-11-11 02:24:38 +0900

    Initial Running Version

---
+++ sector-count-using-func.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import math as math
+import matplotlib.pyplot as plt
+import numpy as np
+
+'''
+This part is to get all the needed input:
+- name (in CAPITAL letter): the name of the sector we need to investigate;
+- duration (in hour): period of time we need to investigate;
+- step(in minute): duration of one time step;
+- DF: the DataFrame that contain the input csv file;
+'''
+
+# get the sector name
+name = input('Please enter a sector name: ')
+
+# get the observation period
+duration = int(input('Please enter the observation period (in hour): ')) * 3600
+
+# get the duration of one time step
+step = int(input('Please enter the length of one time step (in minute): ')) * 60
+
+# import csv data
+DF = pd.read_csv("input.csv", sep=',')
+
+'''
+This part defines all needed functions for the program
+parameters in the function, need to be synchronized:
+- d: duration of observed period
+- st: duration of one time step
+- nst: total time step
+- df: DataFrame name
+- sn: sector name
+- idx: list containing index
+- count: list that contains sector count for each time step
+'''
+
+# calculate the number of time steps
+'''
+def no_of_step(d, st):
+    t_step = int(d / st) + 1
+    return t_step
+'''
+
+
+# filtered data and write it to a new DataFrame
+def fil_df(df, sn):
+    idx = df[df.sectorname.str.contains(sn, na=False)].index.tolist()
+    filtered_df = pd.DataFrame(columns=df.columns)
+    for i in idx:
+        temp1 = df.iloc[[max(0, i - 1), i, min(df.index[-1], i + 1)]]
+        filtered_df = filtered_df.append(temp1, ignore_index=True)
+    return filtered_df
+
+
+# convert data
+def transpose_df(df):
+    tdf = pd.DataFrame(columns=['plane_name',
+                                'begin_time',
+                                'leaving_time',
+                                'previous_sector',
+                                'current_sector',
+                                'next_sector'])
+
+    for row in range(1, len(df.index), 3):
+        temp = pd.DataFrame([[df.at[row, 'planename'],
+                              df.at[row, 'crosstime'],
+                              df.at[min(row + 1, len(df.index) - 1), 'crosstime'],
+                              df.at[row - 1, 'sectorname'],
+                              df.at[row, 'sectorname'],
+                              df.at[min(row + 1, len(df.index) - 1), 'sectorname']]], columns=tdf.columns)
+        tdf = tdf.append(temp, ignore_index=True)
+    return tdf
+
+
+# function to calculate sector count
+def sector_count(df, d, st):
+    nst = int(d / st) + 1
+    count = [0] * nst
+    for line in df.index:
+        if df.at[line, 'begin_time'] > d:
+            pass
+        elif df.at[line, 'begin_time'] == d:
+            count[nst - 1] = count[nst - 1] + 1
+        elif df.at[line, 'begin_time'] < d:
+            if int(df.at[line, 'begin_time']/st) == int(df.at[line, 'leaving_time']/st):
+                pass
+            else:
+                for i in range(math.ceil(df.at[line, 'begin_time']/st),
+                               min(nst, math.floor(df.at[line, 'leaving_time']/st) + 1)):
+                    count[i] = count[i] + 1
+    return count
+
+
+# output to csv file
+def output_csv_no_index(df, sn):
+    df.to_csv('result-' + sn + '.csv', index=False)
+
+
+# write data to the new DataFrame
+x = fil_df(DF, name)
+# print(x)
+
+# convert data
+y = transpose_df(x)
+
+# sector count
+z = sector_count(y, duration, step)
+
+output_csv_no_index(y, name)
+
+# plotting
+plt.plot(z)
+plt.xlabel('Time')
+plt.ylabel('Sector count [number of aircraft]')
+plt.title('Number of aircrafts in Sector ' + name)
+plt.axis([0, len(z), 0, max(z) + 1])
+plt.yticks(np.arange(0, max(z) + 1, 1))
+# additional plotting parameters
+plt.grid()
+# plt.xticks(np.arange(0, len(sector_count), 5))
+# plt.setp(plt.gca().get_xticklabels(), rotation=45, ha="right")
+plt.show()
--- sector-detection-improve.py
+++ sector-detection-improve.py
@@ -7,18 +7,19 @@ import numpy as np
 name = input('Please enter a sector name: ')
 
 # get the observation period
-duration = int(input('Please enter the observation period (in hour): '))*3600
+d = int(input('Please enter the observation period (in hour): ')) * 60
+duration = d * 60
 
 # get the duration of one time step
 step = int(input('Please enter the length of one time step (in minute): '))
-step_sec = step*60
+step_sec = step * 60
 
 # calculate the total step
-n_step = int(720/step) + 1
+n_step = int(d / step) + 1
 print('Total number of time step:', n_step)
 
 # create the list that contains the sector count for each time step
-sector_count = [0]*n_step
+sector_count = [0] * n_step
 
 # import csv data
 DF = pd.read_csv("input.csv", sep=',')
@@ -31,8 +32,7 @@ DF_filtered = pd.DataFrame(columns=DF.columns)
 
 # write data to the new filtered data frame
 for i in idx:
-    # print(DF.iloc[[i - 1, i, i + 1]])
-    temp1 = DF.iloc[[max(0, i-1), i, min(DF.index[-1], i+1)]]
+    temp1 = DF.iloc[[max(0, i - 1), i, min(DF.index[-1], i + 1)]]
     DF_filtered = DF_filtered.append(temp1, ignore_index=True)
 # print(DF_filtered, '\n')
 
@@ -46,14 +46,14 @@ DF_trans = pd.DataFrame(columns=['plane_name',
 
 # write data to the transpose data frame
 for row in range(1, len(DF_filtered.index), 3):
-    # if row % 2 == 1:
-        temp2 = pd.DataFrame([[DF_filtered.at[row, 'planename'],
-                             DF_filtered.at[row, 'crosstime'],
-                             DF_filtered.at[row+1, 'crosstime'],
-                             DF_filtered.at[row-1, 'sectorname'],
-                             DF_filtered.at[row, 'sectorname'],
-                             DF_filtered.at[row+1, 'sectorname']]], columns=DF_trans.columns)
-        DF_trans = DF_trans.append(temp2, ignore_index=True)
+    temp2 = pd.DataFrame([[DF_filtered.at[row, 'planename'],
+                           DF_filtered.at[row, 'crosstime'],
+                           DF_filtered.at[row + 1, 'crosstime'],
+                           DF_filtered.at[row - 1, 'sectorname'],
+                           DF_filtered.at[row, 'sectorname'],
+                           DF_filtered.at[row + 1, 'sectorname']]], columns=DF_trans.columns)
+    DF_trans = DF_trans.append(temp2, ignore_index=True)
+# DF_trans.to_csv('transpose_data.csv', index=False)
 # print(DF_trans)
 
 # calculate sector count
@@ -61,26 +61,24 @@ for line in DF_trans.index:
     if DF_trans.at[line, 'begin_time'] > duration:
         pass
     elif DF_trans.at[line, 'begin_time'] == duration:
-        sector_count[n_step] = sector_count[n_step] + 1
-    elif DF_trans.at[line, 'begin_time'] < duration:
-        if int(DF_trans.at[line, 'begin_time']/step_sec) == int(DF_trans.at[line, 'leaving_time']/step_sec):
-            pass
-        else:
-            for i in range(math.ceil(DF_trans.at[line, 'begin_time']/step_sec),
-                           math.floor(DF_trans.at[line, 'leaving_time']/step_sec)+1):
-                sector_count[i] = sector_count[i] + 1
+        sector_count[n_step - 1] = sector_count[n_step - 1] + 1
+    else:
+        for i in range(math.ceil(DF_trans.at[line, 'begin_time'] / step_sec),
+                       min(math.floor(DF_trans.at[line, 'leaving_time'] / step_sec) + 1, n_step)):
+            sector_count[i] = sector_count[i] + 1
 print('Total number of time step check:', len(sector_count), '\n')
-# print(sector_count)
+print(sector_count)
 
 # plotting
 plt.plot(sector_count)
 plt.xlabel('Time')
 plt.ylabel('Sector count [number of aircraft]')
-plt.title('Experiment 1 results')
+plt.title('Number of aircrafts in Sector '+name)
 plt.axis([0, len(sector_count), 0, max(sector_count)+1])
 plt.yticks(np.arange(0, max(sector_count)+1, 1))
 # additional plotting parameters
-# plt.grid()
+plt.grid()
 # plt.xticks(np.arange(0, len(sector_count), 5))
 # plt.setp(plt.gca().get_xticklabels(), rotation=45, ha="right")
 plt.show()
+
